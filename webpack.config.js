fcd6fb5abfa61858303c81e72c3687f0a5bf3c10
Yann Klein <yann.klein@me.com>
2020-10-28 11:21:27 +0900

    git action config

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, "./lib/clearbit.js"),
+  entry: "./lib/clearbit.js",
+  mode: "development",
   output: {
-    filename: '[name].[contentHash].js',
-    path: path.resolve(__dirname, 'docs')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.txt$/,
-        use: 'raw-loader'
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              esModule: false,
-              name: '[name].[ext]',
-              outputPath: 'images/',
-              publicPath: '/images/'
-            }
-          }
-        ]
-      },
-      // {
-      //   test: /\.js$/,
-      //   exclude: /(node_modules)/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: ['@babel/preset-env']
-      //     }
-      //   }
-      // }
-    ]
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
   },
   devtool: "sourcemap"
 };
